9d09fdd853fb4303698d836a4ab63eda1dadceeb
Boran Göher <boran.goher@innovance.com.tr>
2023-01-24 14:28:24 +0300

    Create project skeleton and implement routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar.js";
+import Home from "./Components/Home.js";
+import Login from "./Components/Login.js";
+import PaymentForm from "./Components/PaymentForm.js";
+import Info from "./Components/Info.js";
 
 function App() {
   return (
-    <div />
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/payment" element={<PaymentForm/>} />
+        <Route path="/information" element={<Info/>} />
+      </Routes>
+    </div>
   );
 }
 
