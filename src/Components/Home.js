144dff6e5f16d1def6937207223fcbb612d69ecc
Boran Göher <boran.goher@innovance.com.tr>
2023-01-24 16:47:28 +0300

    Create homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,32 @@
+import { Button, Container, Typography, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 import React from "react";
 
 const Home = () => {
-    return (<div />);
-}
+  return (
+    <Container maxWidth="md" sx={{ justifyContent: "center" }}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap={4}
+        justifyContent="center"
+        alignItems="center"
+        minHeight="90vh"
+      >
+        <Typography align="center" variant="h2">
+          Welcome to Payment Service!
+        </Typography>
+        <Typography align="center" variant="h5">
+          Welcome to Payment Service. You can handle your payments through this
+          website. Please log in to continue.
+        </Typography>
+        <Button size="large" variant="contained" component={Link} to="/login">
+          {" "}
+          Log In
+        </Button>
+      </Box>
+    </Container>
+  );
+};
 
 export default Home;
