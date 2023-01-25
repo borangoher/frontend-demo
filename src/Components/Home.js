91e6a6a209e1e8184e2e29060ef7457551e6d775
Boran Göher <boran.goher@innovance.com.tr>
2023-01-25 15:37:06 +0300

    Replace arrays in render with fragments

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,34 +16,36 @@ const Home = () => {
         alignItems="center"
         minHeight="90vh"
       >
-        {isLoggedIn
-          ? [
-              <Typography align="center" variant="h2">
-                Welcome to Payment Service!
-              </Typography>,
-              <Typography align="center" variant="h5">
-                Welcome to Payment Service. You can handle your payments through
-                this website. You are currently logged in.
-              </Typography>,
-            ]
-          : [
-              <Typography align="center" variant="h2">
-                Welcome to Payment Service!
-              </Typography>,
-              <Typography align="center" variant="h5">
-                Welcome to Payment Service. You can handle your payments through
-                this website. Please log in to continue.
-              </Typography>,
-              <Button
-                size="large"
-                variant="contained"
-                component={Link}
-                to="/login"
-              >
-                {" "}
-                Log In
-              </Button>,
-            ]}
+        {isLoggedIn ? (
+          <>
+            <Typography align="center" variant="h2">
+              Welcome to Payment Service!
+            </Typography>
+            <Typography align="center" variant="h5">
+              Welcome to Payment Service. You can handle your payments through
+              this website. You are currently logged in.
+            </Typography>
+          </>
+        ) : (
+          <>
+            <Typography align="center" variant="h2">
+              Welcome to Payment Service!
+            </Typography>
+            <Typography align="center" variant="h5">
+              Welcome to Payment Service. You can handle your payments through
+              this website. Please log in to continue.
+            </Typography>
+            <Button
+              size="large"
+              variant="contained"
+              component={Link}
+              to="/login"
+            >
+              {" "}
+              Log In
+            </Button>
+          </>
+        )}
       </Box>
     </Container>
   );
