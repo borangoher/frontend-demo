4ae42eeb5df55fdc0f06190d8add6fe756b67e46
Boran Göher <boran.goher@innovance.com.tr>
2023-01-25 09:44:09 +0300

    Create login context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home.js";
 import Login from "./Components/Login.js";
 import PaymentForm from "./Components/PaymentForm.js";
 import Info from "./Components/Info.js";
+import LoginContext from "./Components/LoginContext.js";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -14,10 +15,12 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/payment" element={<PaymentForm />} />
-        <Route path="/information" element={<Info />} />
+        <LoginContext.Provider>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/payment" element={<PaymentForm />} />
+          <Route path="/information" element={<Info />} />
+        </LoginContext.Provider>
       </Routes>
     </div>
   );
