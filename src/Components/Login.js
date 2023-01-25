a729499d89a5f9123a67b24ef63540e84ef7162b
Boran Göher <boran.goher@innovance.com.tr>
2023-01-25 14:48:53 +0300

    Added simple form validation to login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,46 +1,93 @@
 import React from "react";
 import { useContext } from "react";
 import LoginContext from "./LoginContext";
-import { Container, Box, TextField, Button, Typography } from "@mui/material";
+import {
+  Container,
+  Box,
+  TextField,
+  Button,
+  Typography,
+  Alert,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const { isLoggedIn, setLogin } = useContext(LoginContext);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = () => setLogin(true);
 
   return (
     <Container maxWidth="md" sx={{ justifyContent: "center" }}>
-      <Box
-        display="flex"
-        flexDirection="column"
-        gap={4}
-        justifyContent="center"
-        alignItems="center"
-        minHeight="90vh"
-      >
-        {isLoggedIn
-          ? [
-              <Typography align="center" variant="h5">
-                You are currently logged in. Would you like to log out?
-              </Typography>,
-              <Button
-                type="submit"
-                variant="contained"
-                onClick={() => setLogin(false)}
-              >
-                Log Out
-              </Button>,
-            ]
-          : [
-              <TextField required type="text" label="Username" />,
-              <TextField required type="password" label="Password" />,
-              <Button
-                type="submit"
-                variant="contained"
-                onClick={() => setLogin(true)}
-              >
-                Log In
-              </Button>,
-            ]}
-      </Box>
+      {isLoggedIn ? (
+        <Box
+          display="flex"
+          flexDirection="column"
+          gap={4}
+          justifyContent="center"
+          alignItems="center"
+          minHeight="90vh"
+        >
+          <Typography align="center" variant="h5">
+            You are currently logged in. Would you like to log out?
+          </Typography>
+          <Button
+            type="submit"
+            variant="contained"
+            onClick={() => setLogin(false)}
+          >
+            Log Out
+          </Button>
+        </Box>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap={4}
+            justifyContent="center"
+            alignItems="center"
+            minHeight="90vh"
+          >
+            <TextField
+              {...register("username", {
+                required: true,
+                minLength: 4,
+                maxLength: 20,
+              })}
+              required
+              type="text"
+              label="Username"
+            />
+            {errors.username && (
+              <Alert severity="error">
+                You must enter a username between 4 and 20 characters!
+              </Alert>
+            )}
+            <TextField
+              {...register("password", {
+                required: true,
+                minLength: 8,
+                maxLength: 16,
+              })}
+              required
+              type="password"
+              label="Password"
+            />
+            {errors.password && (
+              <Alert severity="error">
+                You must enter a password between 8 and 16 characters!
+              </Alert>
+            )}
+            <Button type="submit" variant="contained">
+              Log In
+            </Button>
+          </Box>
+        </form>
+      )}
     </Container>
   );
 };
