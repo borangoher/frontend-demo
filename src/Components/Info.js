ad4480da2ca95d1edd5ffbcd6957abd456dddce6
Boran Göher <boran.goher@innovance.com.tr>
2023-01-25 09:16:51 +0300

    Standardize formatting

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Info = () => {
-    return (<div />);
-}
+  return <div />;
+};
 
 export default Info;
--- src/Components/PaymentForm.js
+++ src/Components/PaymentForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PaymentForm = () => {
-    return (<div />);
-}
+  return <div />;
+};
 
 export default PaymentForm;
