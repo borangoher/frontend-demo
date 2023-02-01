792dbac3a8ac0004fa2fc7da5064d7acfdaaebba
Boran Göher <boran.goher@innovance.com.tr>
2023-02-01 11:24:04 +0300

    Add Turkish translations

--- src/i18n.js
+++ src/i18n.js
@@ -61,7 +61,47 @@ i18n
       tr: {
         translation: {
           navbar: {
-            part1: "Giriş yap",
+            part1: "Giriş Yap",
+            part2: "Ödeme",
+            part3: "Bilgi",
+          },
+          home: {
+            title: "Payment Service'e hoş geldiniz!",
+            desc_notLoggedIn:
+              "Payment Service'e hoş geldiniz. Ödemelerinizi bu site aracılığıyla yürütebilirsiniz. Lütfen devam etmek için giriş yapın.",
+            desc_LoggedIn:
+              "Payment Service'e hoş geldiniz. Ödemelerinizi bu site aracılığıyla yürütebilirsiniz. Şu anda giriş yapmış durumdasınız.",
+            login: "Giriş Yap",
+          },
+          info: {
+            title: "Payment Service'e hoş geldiniz!",
+            content:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere metus ipsum, a ornare erat pulvinar et. Nulla sodales odio nec porttitor condimentum. Nunc a diam metus. Proin quis turpis tempus, venenatis nibh at, lobortis augue. Sed vel nisl id libero vehicula convallis nec non arcu. Donec imperdiet pulvinar volutpat. Fusce eu velit risus. Donec vitae fringilla justo. Aliquam erat volutpat. Duis dui mauris, efficitur non commodo ut, rutrum vel nulla. Quisque eu efficitur felis, at mattis ipsum.",
+          },
+          login: {
+            loggedIn:
+              "Şu anda giriş yapmış durumdasınız. Çıkış yapmak ister misiniz?",
+            logout: "Çıkış Yap",
+            username: "Kullanıcı Adı",
+            password: "Şifre",
+            login: "Giriş Yap",
+          },
+          payment: {
+            cardholderName: "Kart Sahibi İsmi",
+            cardNumber: "Kart Numarası",
+            expiryDate: "Son Geçerlilik Tarihi",
+            securityNumber: "Güvenlik Kodu",
+            accountNumber: "Hesap Numarası",
+            amount: "Miktar",
+            displaySenderName: "Gönderici ismini gösterme",
+            useService: "Bu transfer için SERVICE kullan",
+            execute: "Transferi",
+            executeNow: "Şimdi gerçekleştir (ekstra ücerlet açığa çıkabilir)",
+            executeTomorrow: "Yarın sabah gerçekleştir",
+            makePayment: "Ödeme Yap",
+            mustLogIn:
+              "Ödeme yapmak için giriş yapmış olmalısınız. Lütfen önce hesabınıza giriş yapınız.",
+            login: "Giriş Yap",
           },
         },
       },
