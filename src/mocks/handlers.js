1c9e447224c240aad4e4f2ca9c3e5a78e29a3329
Boran Göher <boran.goher@innovance.com.tr>
2023-02-07 13:26:41 +0300

    Remove console.log

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,8 +2,6 @@ import { rest } from "msw";
 
 export const handlers = [
   rest.post("http://localhost:8080/login", async (req, res, ctx) => {
-    console.log(req);
-
     return res(
       ctx.delay(1000), //simulate API response delay
       ctx.status(200)
